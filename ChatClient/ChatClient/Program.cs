c838e312a8e762ecb3f0af73430c96fe234fc6d4
t0ksss <toxambayev2807@gmail.com>
2021-01-17 22:31:53 +0600

    new

---
+++ ChatClient/ChatClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ChatClient.Protos;
+using Grpc.Net.Client;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            var username = Console.ReadLine();
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5001");
+            var client = new ChatService.ChatServiceClient(channel);
+
+            using (var chat = client.Join())
+            {
+                _ = Task.Run(async () =>
+                {
+                    while (await chat.ResponseStream.MoveNext(cancellationToken: CancellationToken.None))
+                    {
+                        var response = chat.ResponseStream.Current;
+                        Console.WriteLine($"{response.User}: {response.Text}");
+                    }
+                });
+
+                await chat.RequestStream.WriteAsync(new Message { User = username, Text = $"{username} has joined the room" });
+
+                string line;
+                while ((line = Console.ReadLine()) != null)
+                {
+                    if (line.ToLower() == "bye")
+                    {
+                        break;
+                    }
+                    await chat.RequestStream.WriteAsync(new Message { User = username, Text = line });
+                }
+                await chat.RequestStream.CompleteAsync();
+            }
+
+            Console.WriteLine("Disconnecting");
+            await channel.ShutdownAsync();
+        }
+    }
+}
---
+++ ChatServer/ChatServer/Services/ChattingService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using ChatServer.Protos;
+using Grpc.Core;
+
+namespace ChatServer.Services
+{
+    public class ChattingService : ChatService.ChatServiceBase
+    {
+        private readonly ChatRoom _chatroomService;
+
+        public ChattingService(ChatRoom chatRoomService)
+        {
+            _chatroomService = chatRoomService;
+        }
+
+        public override async Task Join(IAsyncStreamReader<Message> requestStream, IServerStreamWriter<Message> responseStream, ServerCallContext context)
+        {
+            if (!await requestStream.MoveNext()) return;
+
+            do
+            {
+                _chatroomService.Join(requestStream.Current.User, responseStream);
+                await _chatroomService.BroadcastMessageAsync(requestStream.Current);
+            } while (await requestStream.MoveNext());
+
+            _chatroomService.Remove(context.Peer);
+
+        }
+    }
+}
